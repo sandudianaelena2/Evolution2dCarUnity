6df045d6187392cc662cd37b27f7dd754074a1a2
BaciFlorin <bacicaflorin@gmail.com>
2020-11-22 10:15:38 +0200

    add WheelSpecifications

---
+++ IA/Assets/Evolution/Specifications/Implementations/WheelSpecifications.cs
@@ -0,0 +1,34 @@
+using Assets.Evolution.Specifications.Implementations.Constraints;
+using Assets.Evolution.Specifications.Interfaces;
+using UnityEngine;
+
+namespace Assets.Evolution.Specifications.Implementations
+{
+    public class WheelSpecifications:ISpecifications
+    {
+        private float _scale;
+        private readonly WheelConstraints.Wheels _wheelNumber;
+        public WheelSpecifications(WheelConstraints.Wheels wheelNumber)
+        {
+            _scale = Random.Range(WheelConstraints.MinScale, WheelConstraints.MaxScale);
+            _wheelNumber = wheelNumber;
+        }
+
+        public void ChangeGameObject(GameObject car)
+        {
+            var backWheel = car.transform.GetChild((int)_wheelNumber).gameObject;
+            var newScale =  new Vector3(_scale, _scale, backWheel.transform.localScale.z);
+            backWheel.transform.localScale = newScale;
+        }
+
+        public void RegenerateValues()
+        {
+            _scale = Random.Range(WheelConstraints.MinScale, WheelConstraints.MaxScale);
+        }
+
+        public void RemoveObject(GameObject car)
+        {
+            Object.Destroy(car.transform.GetChild(1).gameObject);
+        }
+    }
+}
